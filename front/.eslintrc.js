a79db0e7e25bbd1a53031846c64a81b66a9cc6e4
NE5054 <w_lan123@naver.com>
2022-02-16 15:17:29 +0900

    chore: (eslintrc) eslint setting

---
+++ front/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        quoteProps: 'consistent',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'auto',
+        printWidth: 120,
+      },
+    ],
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', 15],
+    'no-restricted-syntax': 'off',
+    'no-continue': 'off',
+    'no-alert': 'off',
+    'max-len': 'off',
+    'import/extensions': 'off',
+  },
+};
