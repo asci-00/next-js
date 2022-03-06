d68a0c844f782c9a780d35dd6f1b178d49b0cf6e
NE5054 <w_lan123@naver.com>
2022-03-06 16:43:20 +0900

    feat: 각 layout 의 access 차단 조건 설정

--- front/layouts/AppLayout.js
+++ front/layouts/AppLayout.js
@@ -1,29 +1,14 @@
 import { logout } from '@actions/index';
-import { Button, Input, Layout, Menu } from 'antd';
+import { Button, Input, Menu } from 'antd';
 import Link from 'next/link';
 import Router from 'next/router';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
+import { StyledFooter, StyledLayout } from '../styles';
 
-const { Footer } = Layout;
 const { Search } = Input;
 
-const StyledFooter = styled(Footer)`
-  padding: 30px;
-  min-height: 200px;
-  line-height: 200px;
-  vertical-align: middle;
-  text-align: center;
-`;
-
-const StyledLayout = styled(Layout)`
-  position: relative;
-  padding: 30px;
-  min-height: 900px;
-`;
-
-const AppLayout = ({ children }) => {
+function AppLayout({ children }) {
   const { isLogin } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
@@ -34,52 +19,54 @@ const AppLayout = ({ children }) => {
   }, [isLogin]);
 
   return (
-    <div>
-      <Menu mode="horizontal">
-        <Menu.Item key="Home">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="Profile">
-          <Link href="/profile">
-            <a>Profile</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="Search">
-          <Search
-            placeholder="input search text"
-            onSearch={() => {}}
-            style={{ width: 200, verticalAlign: 'middle' }}
-            enterButton
-          />
-        </Menu.Item>
-        {!isLogin ? (
-          <>
-            <Menu.Item key="Login">
-              <Link href="/auth/login">
-                <a>Login</a>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="Signup">
-              <Link href="/auth/signup">
-                <a>Signup</a>
-              </Link>
-            </Menu.Item>
-          </>
-        ) : (
-          <Menu.Item key="Signup">
-            <Button onClick={onLogout}>
-              <a>Logout</a>
-            </Button>
+    isLogin && (
+      <div>
+        <Menu mode="horizontal">
+          <Menu.Item key="Home">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
           </Menu.Item>
-        )}
-      </Menu>
+          <Menu.Item key="Profile">
+            <Link href="/profile">
+              <a>Profile</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="Search">
+            <Search
+              placeholder="input search text"
+              onSearch={() => {}}
+              style={{ width: 200, verticalAlign: 'middle' }}
+              enterButton
+            />
+          </Menu.Item>
+          {!isLogin ? (
+            <>
+              <Menu.Item key="Login">
+                <Link href="/auth/login">
+                  <a>Login</a>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="Signup">
+                <Link href="/auth/signup">
+                  <a>Signup</a>
+                </Link>
+              </Menu.Item>
+            </>
+          ) : (
+            <Menu.Item key="Signup">
+              <Button onClick={onLogout}>
+                <a>Logout</a>
+              </Button>
+            </Menu.Item>
+          )}
+        </Menu>
 
-      <StyledLayout>{children}</StyledLayout>
-      <StyledFooter>@Copyright asci</StyledFooter>
-    </div>
+        <StyledLayout>{children}</StyledLayout>
+        <StyledFooter>@Copyright asci</StyledFooter>
+      </div>
+    )
   );
-};
+}
 
 export default AppLayout;
--- front/layouts/AuthLayout.js
+++ front/layouts/AuthLayout.js
@@ -1,37 +1,9 @@
-import { Card, Input, Layout } from 'antd';
 import Router from 'next/router';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import styled from 'styled-components';
+import { StyledCard, StyledFooter, StyledLoginForm } from '../styles';
 
-const { Content, Footer } = Layout;
-const { Search } = Input;
-
-const StyledFooter = styled(Footer)`
-  padding: 30px;
-  min-height: 200px;
-  line-height: 200px;
-  vertical-align: middle;
-  text-align: center;
-`;
-
-const StyledContent = styled(Content)`
-  position: relative;
-  padding: 30px;
-  min-height: 900px;
-  line-height: 900px;
-  text-align: center;
-`;
-
-const StyledCard = styled(Card)`
-  display: inline-block;
-  margin: 10px auto;
-  background: #eee;
-  padding: 30px;
-  width: 700px;
-`;
-
-const AuthLayout = ({ children, title }) => {
+function AuthLayout({ children, title }) {
   const { isLogin } = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -39,13 +11,15 @@ const AuthLayout = ({ children, title }) => {
   }, [isLogin]);
 
   return (
-    <div>
-      <StyledContent>
-        <StyledCard title={title}>{children}</StyledCard>
-      </StyledContent>
-      <StyledFooter>@Copyright asci</StyledFooter>
-    </div>
+    !isLogin && (
+      <div>
+        <StyledLoginForm>
+          <StyledCard title={title}>{children}</StyledCard>
+        </StyledLoginForm>
+        <StyledFooter>@Copyright asci</StyledFooter>
+      </div>
+    )
   );
-};
+}
 
 export default AuthLayout;
