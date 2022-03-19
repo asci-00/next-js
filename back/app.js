caab23ab3f169663c24410309bd5d00583ec40aa
NE5054 <w_lan123@naver.com>
2022-03-19 12:35:20 +0900

    feat: sequelize를 통한 db handling

--- back/app.js
+++ back/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const db = require('./models');
 
 const app = express();
 
 const postRouter = require('./routes/post');
 
 app.use('/post', postRouter);
+db.sequelize.sync().then(() => console.log('sequelize sync'));
 
 app.listen(3065, () => {
   console.log('Server is running');
